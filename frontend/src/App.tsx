ee5e381b5ca0faec8e050d084ecb60d059b1e7a1
janmlakar <jan.mlakar99@gmail.com>
2023-06-05 13:06:50 +0200

    footer popravek

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './modules/canvas/footer';
+import Footer from './modules/canvas/Footer';
 
 function App() {
   return (
