ad2385accc038581ac5fb2b9cd1ccf4e89b4e158
janmlakar <jan.mlakar99@gmail.com>
2023-06-05 13:16:55 +0200

    menu template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Foot from './modules/canvas/Foot';
+import Foot from './modules/main/Foot';
+import Menu from './modules/main/Menu';
 
 function App() {
   return (
     <div className="App">
       <h1>Know It</h1>
+      <Menu></Menu>
       <Foot></Foot>
     </div>
   );
